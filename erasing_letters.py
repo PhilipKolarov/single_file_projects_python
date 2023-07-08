9215023a78d0404f4a47293d564e14963277893f
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-07-08 12:19:57 +0300

    Update erasing_letters.py

--- erasing_letters.py
+++ erasing_letters.py
@@ -10,5 +10,21 @@ def erase_every_second_character(input_string):
     return output_string
 
 
-result = erase_every_second_character(input())
-print(result)
+first_result = erase_every_second_character('HOi-!') #Hi!
+print(first_result)
+
+
+def erase_every_third_character(input_string):
+    output_string = ''
+
+    for i in range(len(input_string)):
+        if (i + 1) % 3 != 0:
+            output_string += input_string[i]
+        else:
+            pass
+
+    return output_string
+
+
+second_result = erase_every_third_character('Go8odBby{e!') #Goodbye!
+print(second_result)
