518220428659c28ee9e2d1a43dcf8bd0cc78225e
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-07-09 13:08:45 +0300

    Update add_to_dictionary.py

--- add_to_dictionary.py
+++ add_to_dictionary.py
@@ -1,9 +1,9 @@
 def add_to_my_dict(my_list):
     for x in range(len(my_list)):
         elements = my_list[x].split(' - ')
-        key = elements[0]
-        value = elements[1]
-        my_dict[key] = value
+        k = elements[0]
+        v = elements[1]
+        my_dict[k] = v
 
 
 my_dict = {}
@@ -11,4 +11,5 @@ input_string = input()
 input_list = input_string.split('|')
 
 add_to_my_dict(input_list)
-print(my_dict)
+for key, value in my_dict.items():
+    print(f"{key} - {value}")
