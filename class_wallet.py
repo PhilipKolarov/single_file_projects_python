5dd2f42d28117494aeb8019a18b61f9694780a02
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-07-11 18:57:55 +0300

    Update class_wallet.py
    Added more functions and an example object demonstration

--- class_wallet.py
+++ class_wallet.py
@@ -5,19 +5,19 @@ class Wallet:
         for key, value in my_dict.items():
             key_as_int = float(key)
             my_sum = value * key_as_int
-            my_list += f'{value} x {key}lv -> {my_sum}lv (total)'
+            my_list.append(f'{value} x {key}lv -> {my_sum}lv (total)')
 
         return my_list
-    
+
     @staticmethod
     def _sum_calc_single(my_dict):
         total = 0
         for key, value in my_dict.items():
             key_as_int = float(key)
             total += value * key_as_int
-        
+
         return total
-        
+
     @staticmethod
     def _sum_calc_both(banknotes, coins):
         total = Wallet._sum_calc_single(banknotes) + Wallet._sum_calc_single(coins)
@@ -36,11 +36,23 @@ class Wallet:
             self.coins[value] += qty
 
     def money_details(self, type):
-        if type == '' or type == 'banknotes':
-            Wallet._details_calc(self.banknotes)
-        if type == '' or type == 'coins':
-            Wallet._details_calc(self.coins)
+        if type == 'all' or type == 'banknotes':
+            banknotes_list = Wallet._details_calc(self.banknotes)
+            print(banknotes_list)
+        if type == 'all' or type == 'coins':
+            coins_list = Wallet._details_calc(self.coins)
+            print(coins_list)
 
     def __repr__(self):
         total = Wallet._sum_calc_both(self.banknotes, self.coins)
         return f'This wallet is {self.color} and contains {total}lv.'
+
+
+my_wallet = Wallet('black', 400)
+my_wallet.add_money('coin', '0.50', 4)
+my_wallet.add_money('coin', '0.02', 7)
+my_wallet.add_money('banknote', '50', 1)
+my_wallet.add_money('banknote', '20', 3)
+my_wallet.add_money('banknote', '5', 3)
+my_wallet.money_details('all')
+print(my_wallet.__repr__())
