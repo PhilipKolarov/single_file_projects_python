fa5fc96757112f31c9ceafa25852b17eb9fbc587
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-07-12 23:51:16 +0300

    Update sum_ints_in_list.py
    Extended functionality of the code

--- sum_ints_in_list.py
+++ sum_ints_in_list.py
@@ -1,11 +1,14 @@
 input_string = input()
 input_as_array = input_string.split('|')
 sum_of_ints = 0
+int_count = 0
 
 for el in input_as_array:
     if el.isnumeric():
         sum_of_ints += int(el)
+        int_count += 1
     else:
         pass
 
-print(f'Total: {sum_of_ints}')
+print(sum_of_ints)
+print(int_count)
