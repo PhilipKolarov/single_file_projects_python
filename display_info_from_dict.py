e273b2e895f29be3b6273994823ceb1fa6682104
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-07-16 18:04:01 +0300

    Update display_info_from_dict.py
    Add completed 'display' operation. More operations await.

--- display_info_from_dict.py
+++ display_info_from_dict.py
@@ -1,5 +1,14 @@
 def info_retriever(dictionary, *args):
-    if args == 'display':
-        pass
+    if args[0] == 'display':
+        result = ''
+        for key, value in dictionary.items():
+            current_string = f'{key} - {value} \n'
+            result += current_string
+        result += '----------'
+        return result
     elif args[0] == 'letter_count':
         pass
+
+
+my_dict = {'A': 'first letter', 'B': 'second letter', 'Z': 'last letter'}
+print(info_retriever(my_dict, 'display'))
