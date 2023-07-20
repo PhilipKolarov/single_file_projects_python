22d59ac0e072bc266933ba54b74bdf31acdb737d
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-07-20 17:05:07 +0300

    Add files via upload

---
+++ Juice/orange_juice.py
@@ -0,0 +1,12 @@
+from class_juice import Juice
+
+
+class OrangeJuice(Juice):
+    def __init__(self, quantity, pulp):
+        super().__init__(quantity, pulp)
+        self.title = 'Orange'
+        self.taste = 'orange'
+
+    def __str__(self):
+        message = f'This {self.taste}" juice is {self.quantity}.'
+
