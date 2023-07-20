b217011ff00cbb7a3adecfa2c30e80663fca4aa6
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-07-20 17:12:27 +0300

    Update class_juice.py
    Add __str__ to parent class, as opposed to individually defining method in subclasses

--- Juice/class_juice.py
+++ Juice/class_juice.py
@@ -2,6 +2,7 @@ class Juice:
     def __init__(self, quantity, pulp):
         self.quantity = quantity
         self.pulp = pulp
+        self.taste = ''
 
     def drink(self, drink_quantity):
         self.quantity -= drink_quantity
@@ -11,3 +12,5 @@ class Juice:
         self.quantity += added_quantity
         return self.quantity
 
+    def __str__(self):
+        message = f'This {self.taste}" juice is {self.quantity}.'
