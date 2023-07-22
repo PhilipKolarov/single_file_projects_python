59b7d3b56555643a70201df876d640f1510c7ff1
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-07-22 12:50:49 +0300

    Update apple_juice.py
    Return __str__, as it is now an abstract method in parent class

--- Juice/apple_juice.py
+++ Juice/apple_juice.py
@@ -2,8 +2,12 @@ from class_juice import Juice
 
 
 class AppleJuice(Juice):
-    def __init__(self, quantity):
-        super().__init__(quantity, False)
+    def __init__(self, quantity, color):
+        super().__init__(quantity, False, color)
         self.title = 'Apple'
         self.taste = 'apple'
+        self.natural_color = color
 
+    def __str__(self):
+        message = f'This {self.taste}" juice is {self.quantity}ml.'
+        return message
