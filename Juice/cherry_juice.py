019aff6d34f2668bb832c0485b750e0bcf0fa1bc
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-07-24 09:12:47 +0300

    Add files via upload

---
+++ Juice/cherry_juice.py
@@ -0,0 +1,13 @@
+from class_juice import Juice
+
+
+class AppleJuice(Juice):
+    def __init__(self, quantity, color):
+        super().__init__(quantity, False, color)
+        self.title = 'Cherry'
+        self.taste = 'cherry'
+        self.natural_color = color
+
+    def __str__(self):
+        message = f'This {self.taste} juice is {self.quantity}ml.'
+        return message
