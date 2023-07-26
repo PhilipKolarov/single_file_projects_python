cc470bc187f54cb6fcbd9ffcf77f3d44f9484b34
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-07-26 20:35:06 +0300

    Update apple_juice.py
    Removed self.density, so as to place it in parent class

--- Juice/apple_juice.py
+++ Juice/apple_juice.py
@@ -7,11 +7,7 @@ class AppleJuice(Juice):
         self.title = 'Apple'
         self.taste = 'apple'
         self.natural_color = color
-        self.density = 1.04
 
     def __str__(self):
         message = f'This {self.taste} juice is {self.quantity}ml.'
         return message
-
-    def show_density(self):
-        return self.density
