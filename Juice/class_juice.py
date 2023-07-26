6d2389e57bccfaf6236ef62e9fca07eaefd1cdd1
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-07-26 20:36:23 +0300

    Update class_juice.py
    Added property density

--- Juice/class_juice.py
+++ Juice/class_juice.py
@@ -5,10 +5,18 @@ class Juice(ABC):
     def __init__(self, quantity, pulp):
         self.quantity = quantity
         self.pulp = pulp
+        if self.pulp:
+            self.density = 1.25
+        else:
+            self.density = 1.04
 
     @property
     def has_pulp(self):
         return self.pulp
+
+    @property
+    def show_density(self):
+        return self.density
 
     def drink(self, drink_quantity):
         self.quantity -= drink_quantity
