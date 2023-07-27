6ff35bc8c22acfb54b8d66ba678bf8e486ffb359
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-07-27 14:46:16 +0300

    Update class_user.py
    Added property "full_name"

--- User/class_user.py
+++ User/class_user.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+
 class User(ABC):
     def __init__(self, username, password, first_name, last_name, gender):
         self.username = username
@@ -8,6 +9,10 @@ class User(ABC):
         self.last_name = last_name
         self.gender = gender
 
+    @property
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+    
     @abstractmethod
     def __repr__(self):
         pass
