803faf1c43173b6711a1de110bd60c25e1d107eb
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-07-28 10:56:01 +0300

    Update class_user.py
    Add history property

--- User/class_user.py
+++ User/class_user.py
@@ -8,10 +8,19 @@ class User(ABC):
         self.first_name = first_name
         self.last_name = last_name
         self.gender = gender
+        self.history = []
 
     @property
     def full_name(self):
         return f'{self.first_name} {self.last_name}'
+
+    @property
+    def view_history(self):
+        result = ''
+        for el in self.history:
+            result += f'{el}\n'
+        result += ' ---------- '
+        return result
     
     @abstractmethod
     def __repr__(self):
