5a1a263ae3e1fb43506f40cf00eac84f3c42322b
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-08-22 09:37:51 +0300

    Add files via upload

---
+++ User/class_registered_user.py
@@ -0,0 +1,17 @@
+from class_user import User
+
+
+class RegisteredUser(User):
+    def __init__(self, username, password, first_name, last_name, gender):
+        super().__init__(username, password, first_name, last_name, gender)
+
+    def __repr__(self):
+        return f"My name is {self.first_name} {self.last_name} and I am a registered user."
+
+    def display_history(self):
+        result = ''
+        for el in self.history:
+            result += f"{el} \n"
+
+        result += ' --- '
+        return result
