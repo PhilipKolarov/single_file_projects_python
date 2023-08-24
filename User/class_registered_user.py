16f45f428c3a1dba4402fd991dedbea5c210bd24
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-08-24 18:58:21 +0300

    Update class_registered_user.py
    Making this class an abstract class, which inherits "User" and will be used for all others except the unregistered users

--- User/class_registered_user.py
+++ User/class_registered_user.py
@@ -1,17 +1,42 @@
+from abc import ABC, abstractmethod
 from class_user import User
 
 
-class RegisteredUser(User):
+class RegisteredUser(User, ABC):
+    GENDER_OPTIONS_MALE = ['male', 'Male', 'Man', 'man', 'M', 'm']
+    GENDER_OPTIONS_FEMALE = ['female', 'Female', 'Woman', 'woman', 'W', 'w']
+
     def __init__(self, username, password, first_name, last_name, gender):
-        super().__init__(username, password, first_name, last_name, gender)
+        super().__init__()
+        self.username = username
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
+        self.gender = self.check_gender(gender)
 
-    def __repr__(self):
-        return f"My name is {self.first_name} {self.last_name} and I am a registered user."
+    @property
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
 
-    def display_history(self):
+    @property
+    def view_history(self):
         result = ''
         for el in self.history:
-            result += f"{el} \n"
-
-        result += ' --- '
+            result += f'{el}\n'
+        result += ' ---------- '
         return result
+
+    def check_gender(self, g):
+        if g in self.GENDER_OPTIONS_MALE:
+            return 'male'
+        elif g in self.GENDER_OPTIONS_FEMALE:
+            return 'female'
+        else:
+            return 'unspecified'
+
+    def delete_history(self):
+        self.history = []
+
+    @abstractmethod
+    def __repr__(self):
+        pass
