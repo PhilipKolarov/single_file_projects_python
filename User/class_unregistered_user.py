28dfbc4aede236efeab1a3f0074bb59cf8dad322
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-08-24 18:57:31 +0300

    Update class_unregistered_user.py
    Making sure unregistered users require no details

--- User/class_unregistered_user.py
+++ User/class_unregistered_user.py
@@ -2,15 +2,12 @@ from class_user import User
 
 
 class UnregisteredUser(User):
-    def __init__(self, username, password, first_name, last_name, gender):
-        super().__init__(username, password, first_name, last_name, gender)
+    def __init__(self):
+        super().__init__()
 
     def __repr__(self):
-        return f"My name is {self.first_name} {self.last_name} and I am an unregistered user."
-
-    def delete_history(self):
-        pass
+        return f"I am an unregistered user."
 
 
-unregistered = UnregisteredUser("phil99", "abc123", "Philip", "Kolarov", "male")
+unregistered = UnregisteredUser()
 print(unregistered.__repr__())
