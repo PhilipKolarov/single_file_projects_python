030b1e7097af8b22f7377e6bea403ae3a38ca22b
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-08-27 18:46:55 +0300

    Update length_checker.py
    Add extra example cases at the end

--- length_checker.py
+++ length_checker.py
@@ -10,7 +10,17 @@ def length_check(min, max, text):
         return 'The text is within range!'
 
 
-text_to_be_checked = 'My name is Philip Kolarov and I am a python developer. I have been programming for two years now.'
-min_length = 20
-max_length = 50
-print(length_check(min_length, max_length, text_to_be_checked))
+first_text_to_be_checked = 'My name is Philip Kolarov and I am a python developer. I have been programming for two years now.'
+first_min_length = 20
+first_max_length = 50
+print(length_check(first_min_length, first_max_length, first_text_to_be_checked))
+
+second_text_to_be_checked = 'His royal highness will see you now.'
+second_min_length = 5
+second_max_length = 10
+print(length_check(second_min_length, second_max_length, second_text_to_be_checked))
+
+third_text_to_be_checked = 'It is a huge honor to be here. I would like to thank every single one of you for showing up.'
+third_min_length = 10
+third_max_length = 15
+print(length_check(third_min_length, third_max_length, third_text_to_be_checked))
