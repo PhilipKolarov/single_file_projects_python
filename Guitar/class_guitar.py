bd722fab7906dfa204acbe62bd1b4b9b2c368b29
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-09-01 09:27:26 +0300

    Update class_guitar.py
    Add extra play and determine methods

--- Guitar/class_guitar.py
+++ Guitar/class_guitar.py
@@ -7,11 +7,22 @@ class Guitar(ABC):
         self.frets = frets
         self.battery = battery
         self.strings_qty = strings_qty
-        self.tuning = self.determine_tuning()
+        self.tuning = self.determine_standard_tuning()
+        self.strings = self.determine_strings_notes()
 
     @abstractmethod
-    def determine_tuning(self):
+    def determine_standard_tuning(self):
         pass
+
+    def determine_strings_notes(self):
+        strings_notes_dict = {}
+
+        for string in self.tuning:
+            strings_notes_dict[string] = []
+
+    def play_frets(self, **kwargs):
+        for string, fret in kwargs.items():
+            pass
 
     @abstractmethod
     def __repr__(self):
