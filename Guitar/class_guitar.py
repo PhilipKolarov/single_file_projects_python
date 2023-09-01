42ef419614fc44e7a40a816eb07d4ae2e0d8fb3f
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-09-01 09:49:16 +0300

    Update class_guitar.py
    Add validator for strings_qty

--- Guitar/class_guitar.py
+++ Guitar/class_guitar.py
@@ -2,13 +2,20 @@ from abc import ABC, abstractmethod
 
 
 class Guitar(ABC):
+    STRING_BORDERS = [4, 9]
 
     def __init__(self, frets, battery, strings_qty):
         self.frets = frets
         self.battery = battery
-        self.strings_qty = strings_qty
+        self.strings_qty = self._is_valid_strings_qty(strings_qty)
         self.tuning = self.determine_standard_tuning()
         self.strings = self.determine_strings_notes()
+
+    def _is_valid_strings_qty(self, strings_qty):
+        string_min = self.STRING_BORDERS[0]
+        string_max = self.STRING_BORDERS[1]
+        if strings_qty < string_min or strings_qty > string_max:
+            raise AttributeError(f'Invalid number of strings! Please try again.')
 
     @abstractmethod
     def determine_standard_tuning(self):
