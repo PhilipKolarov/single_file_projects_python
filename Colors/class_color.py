25dd9ecc9ed9038adfb1e96a1be5f28bbe4dd077
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-09-10 11:47:45 +0300

    Update class_color.py
    Add validator for __init__ values

--- Colors/class_color.py
+++ Colors/class_color.py
@@ -2,10 +2,19 @@ from abc import ABC, abstractmethod
 
 
 class Color(ABC):
+    MAX_VALUE = 255
+    MIN_VALUE = 0
+
     def __init__(self, red, green, blue):
-        self.red = red
-        self.green = green
-        self.blue = blue
+        self.red = self.color_value_range_checker(red)
+        self.green = self.color_value_range_checker(green)
+        self.blue = self.color_value_range_checker(blue)
+
+    def color_value_range_checker(self, value):
+        if self.MAX_VALUE >= value >= self.MIN_VALUE:
+            return value
+
+        raise ValueError(f'The value of all colors must be at least {self.MIN_VALUE} and at most {self.MAX_VALUE}!')
 
     def __repr__(self):
         pass
