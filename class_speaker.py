864c4d21d0f0af055b695cca434c5ebac29c0771
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-09-13 09:44:16 +0300

    Update class_speaker.py
    Modify "increase_volume" and "decrease_volume", as well as add a protected method "_volume_validator"

--- class_speaker.py
+++ class_speaker.py
@@ -7,15 +7,22 @@ class Speaker:
         self.current_volume = 0
 
     def increase_volume(self, value):
-        self.current_volume += value
+        possible = self._volume_validator(value)
+        if possible:
+            return possible
+        else:
+            self.current_volume += value
 
     def decrease_volume(self, value):
-        self.current_volume -= value
+        if self.current_volume - value < 0:
+            self.current_volume = 0
+        else:
+            self.current_volume -= value
+
+    def _volume_validator(self, change):
+        if self.current_volume + change > self.max_volume:
+            return f'The volume cannot cannot exceed {self.max_volume}!'
+        return None
 
     def __repr__(self):
         return f'This {self.model} speaker is currently at {self.current_volume}db.'
-
-
-my_speaker = Speaker('Mc Farlow', 30, 90)
-my_speaker.increase_volume(50)
-print(my_speaker.__repr__())
