2013caa88998ea5725147bca0c2cd471af799141
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-09-14 23:06:57 +0300

    Update class_traffic_lights.py
    Add activate and deactivate methods

--- class_traffic_lights.py
+++ class_traffic_lights.py
@@ -1,4 +1,9 @@
+import time
+
+
 class TrafficLights:
+    ORDER = ['RED - Stop!', 'RED & YELLOW - Ready!', 'GREEN - Go!', 'YELLOW - Slow Down!']
+
     def __init__(self, red_time, switch_time, green_time):
         self.red_time = red_time
         self.switch_time = switch_time
@@ -7,7 +12,18 @@ class TrafficLights:
 
     def activate(self):
         self.activated = True
+        while self.activated:
+            print(self.ORDER[0])
+            time.sleep(self._determine_sleep_length(self.ORDER[0]))
+            self.ORDER = self.ORDER[1:] + [self.ORDER[0]]
 
     def deactivate(self):
         self.activated = False
 
+    def _determine_sleep_length(self, current_state):
+        if current_state == 'RED - Stop!':
+            return self.red_time
+        elif current_state == 'RED & YELLOW - Ready!' or current_state == 'YELLOW - Slow Down!':
+            return self.switch_time
+        elif current_state == 'GREEN - Go!':
+            return self.green_time
