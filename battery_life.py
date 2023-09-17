2f995d56dc837775cd11e7dd1238aa30103bb53c
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-09-17 09:17:39 +0300

    Add files via upload

---
+++ battery_life.py
@@ -0,0 +1,10 @@
+class Battery:
+    def __init__(self, model, rate):
+        self.model = model
+        self.rate = rate
+        self.power = 100
+
+    def use_battery(self, seconds):
+        used_power = self.rate * seconds
+        self.power -= used_power
+        return f'{used_power} power used;\n{self.power} remaining'
