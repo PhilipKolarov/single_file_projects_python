0492db47885359235782e7d7d6bf4c0550fa97df
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-09-24 09:33:29 +0300

    Update class_compass.py
    Corrected "turn" method, added "current_direciton" and example case at the bottom

--- class_compass.py
+++ class_compass.py
@@ -9,8 +9,37 @@ class Compass:
         if direction == 'right' or direction == 'clockwise':
             self.current_degrees += degrees
         elif direction == 'left' or direction == 'anticlockwise':
-            self.current_degrees = abs(self.current_degrees - degrees)
+            self.current_degrees = self.current_degrees - degrees
         else:
             return 'Direction not compatible! Must be one of the following: right, clockwise, left, anticlockwise.'
 
         self.current_degrees %= 360
+        if self.current_degrees < 0:
+            self.current_degrees = 360 - self.current_degrees
+        return f'Compass turned successfully'
+
+    def current_direction(self):
+        if self.current_degrees < 22.5 or self.current_degrees >= 337.5:
+            return 'North'
+        elif 22.5 <= self.current_degrees < 67.5:
+            return 'Northeast'
+        elif 67.5 <= self.current_degrees < 112.5:
+            return 'East'
+        elif 112.5 <= self.current_degrees < 157.5:
+            return 'Southeast'
+        elif 157.5 <= self.current_degrees < 202.5:
+            return 'South'
+        elif 202.5 <= self.current_degrees < 247.5:
+            return 'Southwest'
+        elif 247.5 <= self.current_degrees < 292.5:
+            return 'West'
+        elif 292.5 <= self.current_degrees < 337.5:
+            return 'Northwest'
+
+
+my_compass = Compass('blue')
+print(my_compass.current_direction())
+print(my_compass.turn(50, 'right'))
+print(my_compass.current_direction())
+print(my_compass.turn(156, 'left'))
+print(my_compass.current_direction())
